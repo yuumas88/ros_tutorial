a051e9eef1e5da8c45e3d8b38cb533f840c9cd30
yuumas88 <121481165+yuumas88@users.noreply.github.com>
2023-09-14 17:53:49 +0900

    update

--- src/sub.cpp
+++ src/sub.cpp
@@ -1,16 +1,25 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "sensor_msgs/Joy.h"
+#include "../include/can_utils.hpp"
+#include "can_plugins/Frame.h"
 
-void chatterCallback(const sensor_msgs::Joy::ConstPtr& msg)
-{
-  ROS_INFO("I heard: [%d]", msg->buttons[2]);
-}
+void chatterCallback(const sensor_msgs::Joy::ConstPtr& msg);
+
+ros::Publisher* chatter; 
 
 int main(int argc, char **argv){
   ros::init(argc, argv, "listener");
   ros::NodeHandle n;
   ros::Subscriber sub = n.subscribe("joy", 1000, chatterCallback);
+  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+  chatter = &chatter_pub;
   ros::spin();
   return 0;
+}
+
+void chatterCallback(const sensor_msgs::Joy::ConstPtr& msg)
+{
+  ROS_INFO("I heard: [%d]", msg->buttons[2]);
+  chatter->publish(msg);
 }
